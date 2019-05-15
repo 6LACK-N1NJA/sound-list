4ff5394b5d338b33aa54df126f73a03c375a05bd
Free Code <freecode@localhost.localdomain>
2019-05-15 06:26:50 -0700

    List view component added, soundcloud service improved

--- src/services/scservice.js
+++ src/services/scservice.js
@@ -1,6 +1,18 @@
+import _ from 'lodash';
+import Sc from 'soundcloud';
+
 class ScloudService (){
   async getDefaultTracks(){
-    return [];
+    const response = () => {return Sc.get('/me/tracks')}
+    if (!response.ok) {
+      throw new Error('ScloudService getDefaultTracks failed, HTTP status ${response.status}');
+    }
+    const data = await response.json();
+    const tracks = _.get(data, 'data.tracks');
+    if (!tracks) {
+      throw new Error('ScloudService getDefaultTracks failed, tracks not returned');
+    }
+    return tracks;
   }
 }
 
